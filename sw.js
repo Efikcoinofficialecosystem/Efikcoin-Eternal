cc8ed9869a4c64d6cd57c6c8e553f57e0249e129
Efikcoin official ecosyste <Efikcoinofficialecosystem@gmail.com>
2026-04-01 09:29:48 +0200

    Add minimal service worker for PWA
    Signed-off-by: Efikcoin official ecosyste <Efikcoinofficialecosystem@gmail.com>

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js – Minimal service worker for PWA
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
